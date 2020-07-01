b0a4d8e2679a130b1ec5837c642c64f8bf0d5780
Joe Li <joe.lea@foxmail.com>
2020-07-01 20:29:43 +0800

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+一些工具
+
+### 内存压测
+docker run -p 8080:8080 -e "JAVA_OPTS=-Xmx2g"  registry.cn-hangzhou.aliyuncs.com/jiu-shu/jvmdemo:latest
--- src/main/java/tech/icoding/jvmdemo/controller/PeakMemoryController.java
+++ src/main/java/tech/icoding/jvmdemo/controller/PeakMemoryController.java
@@ -3,6 +3,7 @@ package tech.icoding.jvmdemo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,25 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/memory")
 public class PeakMemoryController {
 	private static final Logger LOG = LoggerFactory.getLogger(PeakMemoryController.class);
-	public ResponseEntity<?> peak(){
+	public ResponseEntity<String> peak(){
 		return ResponseEntity.ok("cool");
 	}
 	
-	@RequestMapping("/peak")
+	@GetMapping("/peak")
 	public void generateOOM() throws Exception {
 		int iteratorValue = 20;
 		LOG.info("\n=================> OOM test started..\n");
 		for (int outerIterator = 1; outerIterator < 20; outerIterator++) {
 			LOG.info("Iteration " + outerIterator + " Free Mem: " + Runtime.getRuntime().freeMemory());
 			int loop1 = 2;
-			int[] memoryFillIntVar = new int[iteratorValue];
+			final int[] memoryFillIntVar = new int[iteratorValue];
 			// feel memoryFillIntVar array in loop..
 			do {
 				memoryFillIntVar[loop1] = 0;
 				loop1--;
 			} while (loop1 > 0);
 			iteratorValue = iteratorValue * 5;
-			System.out.println("\nRequired Memory for next loop: " + iteratorValue);
+			LOG.info("\nRequired Memory for next loop: " + iteratorValue);
 			Thread.sleep(1000);
 		}
 	}
